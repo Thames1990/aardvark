e355c94175352f6e90922949b6d547bd9b59771a
Thomas Mohr <ThomasMohr1990@gmail.com>
2017-08-29 17:05:20 +0200

    Add ImageUtil
    This util class is used to calculate dp from pixel and to load vector drawables as BitMapDescriptors, which are used for Google Map marker icons.

---
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/util/ImageUtil.java
@@ -0,0 +1,43 @@
+package de.uni_marburg.mathematik.ds.serval.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.DrawableRes;
+import android.support.v4.content.res.ResourcesCompat;
+import android.util.TypedValue;
+
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
+/**
+ * Created by thames1990 on 29.08.17.
+ */
+public class ImageUtil {
+
+    /**
+     * Converting dp to pixel
+     */
+    public static int dpToPx(int dp, Context context) {
+        return Math.round(TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP,
+                dp,
+                context.getResources().getDisplayMetrics()
+        ));
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    public static BitmapDescriptor getBitmapDescriptor(@DrawableRes int resId, Context context) {
+        Drawable vectorDrawable = ResourcesCompat.getDrawable(context.getResources(), resId, null);
+        Bitmap bitmap = Bitmap.createBitmap(
+                vectorDrawable.getIntrinsicWidth(),
+                vectorDrawable.getIntrinsicHeight(),
+                Bitmap.Config.ARGB_8888
+        );
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+}
