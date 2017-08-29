d2eafe286eb7c67cadd081cfffc4f6762f670e8f
Thomas Mohr <ThomasMohr1990@gmail.com>
2017-08-29 17:08:45 +0200

    Add events fragment
    This is used to show the last occurred events. Currently its set to a fixed value of events, but in the future, the user is able to see all events, or pick custom filters, such as time or measurements. This view was previously shown as the main content.

---
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/view/fragments/EventsFragment.java
@@ -0,0 +1,93 @@
+package de.uni_marburg.mathematik.ds.serval.view.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.uni_marburg.mathematik.ds.serval.R;
+import de.uni_marburg.mathematik.ds.serval.controller.GenericEventAdapter;
+import de.uni_marburg.mathematik.ds.serval.model.Event;
+import de.uni_marburg.mathematik.ds.serval.model.GenericEvent;
+
+import static android.support.v7.widget.DividerItemDecoration.VERTICAL;
+
+/**
+ * Created by thames1990 on 28.08.17.
+ */
+public class EventsFragment<T extends Event> extends Fragment {
+
+    public static final String EVENTS = "EVENTS";
+
+    private List<T> events;
+
+    private GenericEventAdapter adapter;
+
+    @BindView(R.id.recycler_view)
+    RecyclerView recyclerView;
+
+    public static <T extends Event> EventsFragment<Event> newInstance(ArrayList<T> events) {
+        EventsFragment<Event> fragment = new EventsFragment<>();
+        Bundle args = new Bundle();
+        args.putParcelableArrayList(EVENTS, events);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (!getArguments().containsKey(EVENTS)) {
+            throw new RuntimeException(String.format(
+                    Locale.getDefault(),
+                    getString(R.string.fragment_must_contain_key_exception),
+                    EVENTS
+            ));
+        }
+        events = getArguments().getParcelableArrayList(EVENTS);
+    }
+
+    private void setupRecyclerView() {
+//        recyclerView.setLayoutManager(new GridLayoutManager(getContext(), 2));
+//        recyclerView.addItemDecoration(new GridSpacingItemDecoration(
+//                2,
+//                ImageUtil.dpToPx(10, getContext()), true)
+//        );
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), VERTICAL));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        // TODO Find a better way
+        if (events.get(0) instanceof GenericEvent) {
+            //noinspection unchecked
+            adapter = new GenericEventAdapter((List<GenericEvent>) events);
+        }
+        recyclerView.setAdapter(adapter);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState
+    ) {
+        View view = inflater.inflate(R.layout.fragment_events, container, false);
+        ButterKnife.bind(this, view);
+        setupRecyclerView();
+        return view;
+    }
+}
