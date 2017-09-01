Thames1990/aardvark

commit 0398b6e3d9619de304d83621bc321f52ff61ec62

Replace WelcomeActivity with IntroActivity
Complete rewrite of all layouts with ConstraintLayout, which respects device rotation way better, and simplify the logic.

Major code cleanup and removal of obsolete resource files, as well as a lot of refactoring and corner case handling.

app/src/main/java/de/uni_marburg/mathematik/ds/serval/view/exceptions/IntroFragmentLayoutDoesNotExistException.java
@@ -1,0 +1,16 @@
+package de.uni_marburg.mathematik.ds.serval.view.exceptions;
+
+/**
+ * Created by thames1990 on 31.08.17.
+ */
+public class IntroFragmentLayoutDoesNotExistException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public IntroFragmentLayoutDoesNotExistException() {
+    }
+
+    public IntroFragmentLayoutDoesNotExistException(String message) {
+        super(message);
+    }
+}

app/src/main/AndroidManifest.xml
@@ -23,7 +23,7 @@
             android:name="com.google.android.geo.API_KEY"
             android:value="@string/google_maps_key"/>
 
-        <activity android:name=".view.activities.WelcomeActivity">
+        <activity android:name=".view.activities.IntroActivity">
             <intent-filter>
                 <action android:name="android.intent.action.MAIN"/>
 
@@ -33,13 +33,12 @@
         <activity
             android:name=".view.activities.MainActivity"
             android:label="@string/title_activity_main"/>
+        <activity android:name=".view.activities.DetailActivity"/>
         <activity
             android:name=".view.activities.SettingsActivity"
             android:label="@string/title_activity_settings"
             android:theme="@style/PreferenceTheme">
         </activity>
-        <activity android:name=".view.activities.IntroActivity"/>
-        <activity android:name=".view.activities.DetailActivity"/>
     </application>
 
 </manifest>

app/src/main/java/de/uni_marburg/mathematik/ds/serval/controller/IntroAdapter.java
@@ -1,10 +1,11 @@
 package de.uni_marburg.mathematik.ds.serval.controller;
 
-import android.graphics.Color;
+import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import de.uni_marburg.mathematik.ds.serval.R;
 import de.uni_marburg.mathematik.ds.serval.view.fragments.IntroFragment;
 
 /**
@@ -12,32 +13,21 @@ import de.uni_marburg.mathematik.ds.serval.view.fragments.IntroFragment;
  */
 public class IntroAdapter extends FragmentPagerAdapter {
 
-    // Update if new slides are added
-    private static final int NUMBER_OF_FRAGMENTS = 4;
+    private int[] introSlideBackgrounds;
 
-    public IntroAdapter(FragmentManager fm) {
+    public IntroAdapter(FragmentManager fm, Context context) {
         super(fm);
+        introSlideBackgrounds = context.getResources().getIntArray(R.array.intro_background);
     }
 
     @Override
     public Fragment getItem(int position) {
-        switch (position) {
-            case 0:
-                return IntroFragment.newInstance(Color.parseColor("#f64c73"), position);
-            case 1:
-                return IntroFragment.newInstance(Color.parseColor("#20d2bb"), position);
-            case 2:
-                return IntroFragment.newInstance(Color.parseColor("#3395ff"), position);
-            case 3:
-                return IntroFragment.newInstance(Color.parseColor("#c873f4"), position);
-            default:
-                return IntroFragment.newInstance(Color.parseColor("#f64c73"), position);
-        }
+        return IntroFragment.newInstance(introSlideBackgrounds[position], position);
     }
 
     @Override
     public int getCount() {
-        return NUMBER_OF_FRAGMENTS;
+        return introSlideBackgrounds.length;
     }
 
 }

app/src/main/java/de/uni_marburg/mathematik/ds/serval/model/exceptions/GeohashMismatchException.java
@@ -5,8 +5,12 @@ package de.uni_marburg.mathematik.ds.serval.model.exceptions;
  */
 public class GeohashMismatchException extends Exception {
 
-    static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
+    public GeohashMismatchException() {
+    }
 
-
+    public GeohashMismatchException(String message) {
+        super(message);
+    }
 }

app/src/main/java/de/uni_marburg/mathematik/ds/serval/model/exceptions/MeasurementTypeWithoutIcon.java
@@ -5,7 +5,7 @@ package de.uni_marburg.mathematik.ds.serval.model.exceptions;
  */
 public class MeasurementTypeWithoutIcon extends Exception {
 
-    static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     public MeasurementTypeWithoutIcon() {
     }

app/src/main/java/de/uni_marburg/mathematik/ds/serval/util/PrefManager.java
@@ -6,7 +6,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.design.widget.BottomSheetDialog;
 
-import de.uni_marburg.mathematik.ds.serval.view.activities.WelcomeActivity;
+import de.uni_marburg.mathematik.ds.serval.view.activities.IntroActivity;
 
 /**
  * Is used to store key/value pairs permanently.
@@ -33,7 +33,7 @@ public class PrefManager {
     /**
      * Key for the boolean, that sets, if the app was started before.
      * <p>
-     * This is used to check whether the {@link WelcomeActivity welcome activity} needs to be
+     * This is used to check whether the {@link IntroActivity intro activity} needs to be
      * started.
      */
     private static final String IS_FIRST_TIME_LAUNCH = "IS_FIRST_TIME_LAUNCH";

app/src/main/java/de/uni_marburg/mathematik/ds/serval/view/activities/IntroActivity.java
@@ -1,19 +1,39 @@
 package de.uni_marburg.mathematik.ds.serval.view.activities;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import de.uni_marburg.mathematik.ds.serval.R;
 import de.uni_marburg.mathematik.ds.serval.controller.IntroAdapter;
+import de.uni_marburg.mathematik.ds.serval.util.PrefManager;
 import de.uni_marburg.mathematik.ds.serval.view.util.IntroPageTransformer;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 /**
  * Alternative intro sliders view.
  */
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity
+        extends AppCompatActivity
+        implements ViewPager.OnPageChangeListener {
+
+    private static final int FADE_OUT_ANIMATION_DURATION = 1000;
+
+    private PrefManager prefManager;
 
     @BindView(R.id.viewpager)
     ViewPager viewPager;
@@ -21,9 +41,87 @@ public class IntroActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Checking if it's the first launch
+        prefManager = new PrefManager(this);
+        if (!prefManager.isFirstTimeLaunch()) {
+            launchHomeScreen();
+            finish();
+        }
+
+        // Making notification bar transparent
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            );
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.TRANSPARENT);
+        }
+
         setContentView(R.layout.intro_layout);
         ButterKnife.bind(this);
-        viewPager.setAdapter(new IntroAdapter(getSupportFragmentManager()));
-        viewPager.setPageTransformer(false, new IntroPageTransformer());
+
+        viewPager.setAdapter(new IntroAdapter(getSupportFragmentManager(), this));
+        viewPager.setPageTransformer(false, new IntroPageTransformer(this));
+        viewPager.addOnPageChangeListener(this);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        if (position == 2) {
+            checkLocationPermission();
+        }
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+
+    }
+
+    private void launchHomeScreen() {
+        prefManager.setIsFirstTimeLaunch(false);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    private void checkLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION) !=
+                        PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    ACCESS_COARSE_LOCATION,
+                    ACCESS_FINE_LOCATION
+            }, MainActivity.CHECK_LOCATION_PERMISSION);
+        }
+    }
+
+    public void startApp(View view) {
+        Animation.AnimationListener listener = new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                launchHomeScreen();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        };
+        Animation fadeOut = new AlphaAnimation(1, 0);
+        fadeOut.setFillAfter(true);
+        fadeOut.setDuration(FADE_OUT_ANIMATION_DURATION);
+        fadeOut.setAnimationListener(listener);
+        view.startAnimation(fadeOut);
     }
 }

app/src/main/java/de/uni_marburg/mathematik/ds/serval/view/activities/MainActivity.java
@@ -254,11 +254,8 @@ public class MainActivity<T extends Event> extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_reset_app:
                 prefManager.setIsFirstTimeLaunch(true);
-                startActivity(new Intent(this, WelcomeActivity.class));
-                ActivityCompat.finishAffinity(this);
-                return true;
-            case R.id.action_alternative_intro:
                 startActivity(new Intent(this, IntroActivity.class));
+                ActivityCompat.finishAffinity(this);
                 return true;
             case R.id.action_settings:
                 startActivity(new Intent(MainActivity.this, SettingsActivity.class));

commit 86f3c65b65665c2b03c5def24901da6c2601d44d

Remove exception, because a placeholder layout was introduced by 0398b6e3d9619de304d83621bc321f52ff61ec62

app/src/main/java/de/uni_marburg/mathematik/ds/serval/view/exceptions/IntroFragmentLayoutDoesNotExistException.java
@@ -1,16 +1,0 @@
-package de.uni_marburg.mathematik.ds.serval.view.exceptions;
-
-/**
- * Created by thames1990 on 31.08.17.
- */
-public class IntroFragmentLayoutDoesNotExistException extends Exception {
-
-    private static final long serialVersionUID = 1L;
-
-    public IntroFragmentLayoutDoesNotExistException() {
-    }
-
-    public IntroFragmentLayoutDoesNotExistException(String message) {
-        super(message);
-    }
-}