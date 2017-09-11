51f372a604582d0b228b10383045dcf996572aa2
Thomas Mohr <ThomasMohr1990@gmail.com>
2017-09-11 12:44:58 +0200

    Disable LeakCanary on release builds

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/Serval.java
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/Serval.java
@@ -9,6 +9,8 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 import com.squareup.leakcanary.LeakCanary;
 import com.squareup.leakcanary.RefWatcher;
 
+import java.util.concurrent.TimeUnit;
+
 import io.fabric.sdk.android.Fabric;
 
 /**
@@ -21,6 +23,41 @@ public class Serval extends Application {
     private RefWatcher refWatcher;
     
     private FirebaseAnalytics firebaseAnalytics;
+    
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
+        installLeakCanary();
+        installCrashlytics();
+        installFirebaseAnalytics();
+    }
+    
+    private void installLeakCanary() {
+        if (BuildConfig.DEBUG) {
+            refWatcher = LeakCanary.refWatcher(this)
+                                   .watchDelay(10, TimeUnit.SECONDS)
+                                   .buildAndInstall();
+        } else {
+            refWatcher = RefWatcher.DISABLED;
+        }
+    }
+    
+    private void installCrashlytics() {
+        fabric = Fabric.with(
+                this,
+                new Crashlytics.Builder().core(
+                        new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()
+                ).build()
+        );
+    }
+    
+    private void installFirebaseAnalytics() {
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        firebaseAnalytics.setAnalyticsCollectionEnabled(false);
+    }
     
     public static Fabric getFabric(Context context) {
         Serval serval = (Serval) context.getApplicationContext();
@@ -35,19 +72,5 @@ public class Serval extends Application {
     public static FirebaseAnalytics getFirebaseAnalytics(Context context) {
         Serval serval = (Serval) context.getApplicationContext();
         return serval.firebaseAnalytics;
-    }
-    
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            return;
-        }
-        CrashlyticsCore core = new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build();
-        fabric = Fabric.with(this, new Crashlytics.Builder().core(core).build());
-        refWatcher = LeakCanary.install(this);
-        firebaseAnalytics = FirebaseAnalytics.getInstance(this);
-        firebaseAnalytics.setAnalyticsCollectionEnabled(false);
     }
 }
