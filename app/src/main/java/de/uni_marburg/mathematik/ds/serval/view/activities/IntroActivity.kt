d3132dbfcc78051aa829c535a72b9208e03b90f7
Thomas Mohr <ThomasMohr1990@gmail.com>
2017-10-05 12:01:09 +0200

    Improve intro by 227%

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/view/activities/IntroActivity.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/view/activities/IntroActivity.kt
@@ -1,50 +1,51 @@
 package de.uni_marburg.mathematik.ds.serval.view.activities
 
+import agency.tango.materialintroscreen.MaterialIntroActivity
+import agency.tango.materialintroscreen.SlideFragmentBuilder
 import android.Manifest
 import android.os.Bundle
-import com.heinrichreimersoftware.materialintro.app.IntroActivity
-import com.heinrichreimersoftware.materialintro.slide.SimpleSlide
+import ca.allanwang.kau.utils.string
 import de.uni_marburg.mathematik.ds.serval.R
 
-
-class IntroActivity : IntroActivity() {
+class IntroActivity : MaterialIntroActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        buttonBackFunction = BUTTON_BACK_FUNCTION_BACK
+        setSkipButtonVisible()
+        enableLastSlideAlphaExitTransition(true)
 
-        addSlide(SimpleSlide.Builder()
-                .title(R.string.intro_1_title)
-                .description(R.string.intro_1_description)
+        addSlide(SlideFragmentBuilder()
+                .title(string(R.string.intro_1_title))
+                .description(string(R.string.intro_1_description))
                 .image(R.drawable.speaker_phone)
-                .background(R.color.intro_1_background)
-                .backgroundDark(R.color.colorPrimaryDark)
+                .backgroundColor(R.color.intro_1_background)
+                .buttonsColor(R.color.colorPrimaryDark)
                 .build())
 
-        addSlide(SimpleSlide.Builder()
-                .title(R.string.intro_2_title)
-                .description(R.string.intro_2_description)
+        addSlide(SlideFragmentBuilder()
+                .title(string(R.string.intro_2_title))
+                .description(string(R.string.intro_2_description))
                 .image(R.drawable.dashboard)
-                .background(R.color.intro_2_background)
-                .backgroundDark(R.color.colorPrimaryDark)
+                .backgroundColor(R.color.intro_2_background)
+                .buttonsColor(R.color.colorPrimaryDark)
                 .build())
 
-        addSlide(SimpleSlide.Builder()
-                .title(R.string.intro_3_title)
-                .description(R.string.intro_3_description)
+        addSlide(SlideFragmentBuilder()
+                .title(string(R.string.intro_3_title))
+                .description(string(R.string.intro_3_description))
                 .image(R.drawable.map)
-                .background(R.color.intro_3_background)
-                .backgroundDark(R.color.colorPrimaryDark)
-                .permission(Manifest.permission.ACCESS_FINE_LOCATION)
+                .backgroundColor(R.color.intro_3_background)
+                .buttonsColor(R.color.colorPrimaryDark)
+                .neededPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION))
                 .build())
 
-        addSlide(SimpleSlide.Builder()
-                .title(R.string.intro_4_title)
-                .description(R.string.intro_4_description)
+        addSlide(SlideFragmentBuilder()
+                .title(string(R.string.intro_4_title))
+                .description(string(R.string.intro_4_description))
                 .image(R.drawable.chat)
-                .background(R.color.intro_4_background)
-                .backgroundDark(R.color.colorPrimaryDark)
+                .backgroundColor(R.color.intro_4_background)
+                .buttonsColor(R.color.colorPrimaryDark)
                 .build())
     }
 }
