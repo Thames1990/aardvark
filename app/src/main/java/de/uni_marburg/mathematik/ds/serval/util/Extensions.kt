e8eb04f89ace3cdfebddf7aa463409c9d49384c0
Thomas Mohr <ThomasMohr1990@gmail.com>
2017-10-05 13:45:39 +0200

    Improve time formatting

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/util/Extensions.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/util/Extensions.kt
@@ -3,49 +3,45 @@ package de.uni_marburg.mathematik.ds.serval.util
 import android.content.Context
 import ca.allanwang.kau.utils.string
 import de.uni_marburg.mathematik.ds.serval.R
-import java.text.DateFormat
-import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
 
 fun Long.timeToString(context: Context): String {
-    val format = SimpleDateFormat.getDateInstance(
-            DateFormat.MEDIUM,
-            Locale.getDefault()
-    )
     when {
         TimeUnit.MILLISECONDS.toMinutes(this) < 60 ->
             return String.format(
                     Locale.getDefault(),
-                    context.getString(R.string.time_minutes_ago),
+                    context.getString(R.string.minutes_ago),
                     TimeUnit.MILLISECONDS.toMinutes(this)
             )
         TimeUnit.MILLISECONDS.toHours(this) < 24 ->
             return String.format(
                     Locale.getDefault(),
-                    context.getString(R.string.time_hours_ago),
+                    context.getString(R.string.hours_ago),
                     TimeUnit.MILLISECONDS.toHours(this)
             )
-        TimeUnit.MILLISECONDS.toDays(this) < 7 ->
+        TimeUnit.MILLISECONDS.toDays(this) < 30 ->
             return String.format(
                     Locale.getDefault(),
-                    context.getString(R.string.time_days_ago),
+                    context.getString(R.string.days_ago),
                     TimeUnit.MILLISECONDS.toDays(this)
             )
-        else -> return format.format(this)
+        TimeUnit.MILLISECONDS.toDays(this) < 365 ->
+            return String.format(
+                    Locale.getDefault(),
+                    context.getString(R.string.months_ago),
+                    TimeUnit.MILLISECONDS.toDays(this).rem(30)
+            )
+        else -> return String.format(
+                Locale.getDefault(),
+                context.getString(R.string.years_ago),
+                TimeUnit.MILLISECONDS.toDays(this).rem(365)
+        )
     }
 }
 
 fun Float.distanceToString(context: Context): String = if (this < 1000) {
-    String.format(
-            Locale.getDefault(),
-            context.string(R.string.location_distance_to_meter),
-            this
-    )
+    String.format(Locale.getDefault(), context.string(R.string.distance_in_meter), this)
 } else {
-    String.format(
-            Locale.getDefault(),
-            context.string(R.string.location_distance_to_kilometer),
-            this / 1000
-    )
+    String.format(Locale.getDefault(), context.string(R.string.distance_in_kilometer), this / 1000)
 }
