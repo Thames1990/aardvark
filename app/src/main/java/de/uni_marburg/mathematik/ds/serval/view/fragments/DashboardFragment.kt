ed39fc8a786358a541385aa1784b574ce23f6a83
Thomas Mohr <ThomasMohr1990@gmail.com>
2017-10-13 00:59:58 +0200

    Minor code cleanup

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/view/fragments/DashboardFragment.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/view/fragments/DashboardFragment.kt
@@ -2,6 +2,7 @@ package de.uni_marburg.mathematik.ds.serval.view.fragments
 
 import de.uni_marburg.mathematik.ds.serval.R
 
+@Suppress("unused")
 class DashboardFragment : BaseFragment() {
 
     override val layout: Int
