73bc55ad18c38d72827fbeb6bb15ac8c0ad3ac8e
Thomas Mohr <ThomasMohr1990@gmail.com>
2017-10-23 00:20:30 +0200

    Minor code cleanup

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/util/Constants.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/util/Constants.kt
@@ -1,16 +1,6 @@
 package de.uni_marburg.mathematik.ds.serval.util
 
 const val INTRO_REQUEST_CODE = 42
-
 const val MAP_PADDING = 150
-
-/**
- * 1: World
- * 5: Continent
- * 10: City
- * 15: Streets
- * 20: Buildings
- */
 const val MAP_ZOOM = 15f
-
 const val WIFI_ADB_PORT = 5555
