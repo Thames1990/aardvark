ee5f685ab02cc5405790bd7be33ee6dd3fe27182
Thomas Mohr <ThomasMohr1990@gmail.com>
2017-12-03 17:55:41 +0100

    Use user defined colors

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/view/activities/AboutActivity.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/view/activities/AboutActivity.kt
@@ -7,14 +7,15 @@ import ca.allanwang.kau.adapters.FastItemThemedAdapter
 import ca.allanwang.kau.iitems.CardIItem
 import com.mikepenz.fastadapter.IItem
 import de.uni_marburg.mathematik.ds.serval.R
+import de.uni_marburg.mathematik.ds.serval.util.Preferences
 import de.uni_marburg.mathematik.ds.serval.util.setCurrentScreen
 
 class AboutActivity : AboutActivityBase(R.string::class.java, {
     cutoutDrawableRes = R.drawable.aardvark
-    textColor = 0xff000000.toInt()
-    backgroundColor = 0xfffafafa.toInt()
-    accentColor = 0xff40c4ff.toInt()
-    cutoutForeground = 0xff18FFFF.toInt()
+    textColor = Preferences.colorText
+    backgroundColor = Preferences.colorBackground
+    accentColor = Preferences.colorAccent
+    cutoutForeground = Preferences.colorPrimary
     faqXmlRes = R.xml.faq
     faqParseNewLine = false
 }) {
