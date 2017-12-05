3b5f0adca64279d21f46653cffd99f72a8313f1f
Thomas Mohr <ThomasMohr1990@gmail.com>
2017-12-05 08:07:45 +0100

    Code Cleanup and bug fixes

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/view/activities/IntroActivity2.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/view/activities/IntroActivity2.kt
@@ -26,20 +26,19 @@ import de.uni_marburg.mathematik.ds.serval.view.fragments.BaseIntroFragment.Intr
 import de.uni_marburg.mathematik.ds.serval.view.fragments.BaseIntroFragment.IntroFragmentWelcome
 import org.jetbrains.anko.find
 
-/** Created by thames1990 on 03.12.17. */
-class IntroActivity2 : KauBaseActivity(), ViewPager.PageTransformer, ViewPager.OnPageChangeListener {
+class IntroActivity2 : KauBaseActivity() {
 
-    val ripple: RippleCanvas by bindView(R.id.intro_ripple)
-    val viewpager: ViewPager by bindView(R.id.intro_viewpager)
-    val skip: Button by bindView(R.id.intro_skip)
-    val next: ImageButton by bindView(R.id.intro_next)
+    private val indicator: InkPageIndicator by bindView(R.id.intro_indicator)
+    private val next: ImageButton by bindView(R.id.intro_next)
+    private val ripple: RippleCanvas by bindView(R.id.intro_ripple)
+    private val skip: Button by bindView(R.id.intro_skip)
+    private val viewpager: ViewPager by bindView(R.id.intro_viewpager)
 
-    lateinit var adapter: IntroPageAdapter
-    val indicator: InkPageIndicator by bindView(R.id.intro_indicator)
+    private lateinit var adapter: IntroPageAdapter
 
     private var barHasNext = true
 
-    val fragments = listOf(
+    private val fragments = listOf(
             IntroFragmentWelcome(),
             IntroAccountFragment(),
             IntroFragmentEnd()
@@ -49,25 +48,71 @@ class IntroActivity2 : KauBaseActivity(), ViewPager.PageTransformer, ViewPager.O
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
         adapter = IntroPageAdapter(supportFragmentManager, fragments)
-        viewpager.apply {
-            setPageTransformer(true, this@IntroActivity2)
-            addOnPageChangeListener(this@IntroActivity2)
-            adapter = this@IntroActivity2.adapter
-        }
+        setupViewPager()
         indicator.setViewPager(viewpager)
-        next.setIcon(GoogleMaterial.Icon.gmd_navigate_next)
-        next.setOnClickListener {
-            if (barHasNext) viewpager.setCurrentItem(viewpager.currentItem + 1, true)
-            else finish(next.x + next.pivotX, next.y + next.pivotY)
+        next.apply {
+            setIcon(GoogleMaterial.Icon.gmd_navigate_next)
+            setOnClickListener {
+                if (barHasNext) viewpager.setCurrentItem(viewpager.currentItem + 1, true)
+                else finish(next.x + next.pivotX, next.y + next.pivotY)
+            }
         }
         skip.setOnClickListener { finish() }
         theme()
     }
 
+    private fun setupViewPager() {
+        viewpager.apply {
+            setPageTransformer(true) { page, position ->
+                // Only apply to adjacent pages
+                if ((position < 0 && position > -1) || (position > 0 && position < 1)) {
+                    val pageWidth = page.width
+                    val translateValue = position * -pageWidth
+                    page.translationX = if (translateValue > -pageWidth) translateValue else 0f
+                    page.alpha = if (position < 0) 1 + position else 1f
+                } else {
+                    page.alpha = 1f
+                    page.translationX = 0f
+                }
+            }
+            addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+                override fun onPageScrollStateChanged(state: Int) = Unit
+
+                override fun onPageScrolled(
+                        position: Int,
+                        positionOffset: Float,
+                        positionOffsetPixels: Int
+                ) {
+                    fragments[position].onPageScrolled(positionOffset)
+                    if (position + 1 < fragments.size) {
+                        fragments[position + 1].onPageScrolled(positionOffset - 1)
+                    }
+                }
+
+                override fun onPageSelected(position: Int) {
+                    fragments[position].onPageSelected()
+                    val hasNext = position != fragments.size - 1
+                    if (barHasNext == hasNext) return
+                    barHasNext = hasNext
+                    next.fadeScaleTransition {
+                        setIcon(
+                                if (barHasNext) GoogleMaterial.Icon.gmd_navigate_next
+                                else GoogleMaterial.Icon.gmd_done,
+                                color = Prefs.textColor
+                        )
+                    }
+                    skip.animate().scaleXY(if (barHasNext) 1f else 0f)
+                }
+
+            })
+            adapter = this.adapter
+        }
+    }
+
     fun theme() {
         ripple.set(Prefs.bgColor)
-        statusBarColor = Prefs.accentColor
-        navigationBarColor = Prefs.accentColor
+        statusBarColor = Prefs.headerColor
+        navigationBarColor = Prefs.headerColor
         skip.setTextColor(Prefs.textColor)
         next.imageTintList = ColorStateList.valueOf(Prefs.textColor)
         indicator.setColour(Prefs.textColor)
@@ -75,26 +120,13 @@ class IntroActivity2 : KauBaseActivity(), ViewPager.PageTransformer, ViewPager.O
         fragments.forEach { it.themeFragment() }
     }
 
-    override fun transformPage(page: View, position: Float) {
-        // Only apply to adjacent pages
-        if ((position < 0 && position > -1) || (position > 0 && position < 1)) {
-            val pageWidth = page.width
-            val translateValue = position * -pageWidth
-            page.translationX = if (translateValue > -pageWidth) translateValue else 0f
-            page.alpha = if (position < 0) 1 + position else 1f
-        } else {
-            page.alpha = 1f
-            page.translationX = 0f
-        }
-    }
-
     fun finish(x: Float, y: Float) {
-        val accent = 0xff82F7FF.toInt()
+        val green = color(R.color.aardvark_green)
         window.setFlags(
                 WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
                 WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
         )
-        ripple.ripple(Prefs.colorPrimary, x, y, 600) {
+        ripple.ripple(green, x, y, 600) {
             postDelayed(1000) { finish() }
         }
         @Suppress("RemoveExplicitTypeArguments")
@@ -119,12 +151,12 @@ class IntroActivity2 : KauBaseActivity(), ViewPager.PageTransformer, ViewPager.O
                 }
             }
         }
-        if (Prefs.accentColor != accent) {
+        if (Prefs.headerColor != green) {
             ValueAnimator.ofFloat(0f, 1f).apply {
                 addUpdateListener {
-                    val c = Prefs.accentColor.blendWith(accent, it.animatedValue as Float)
-                    statusBarColor = c
-                    navigationBarColor = c
+                    val color = Prefs.headerColor.blendWith(green, it.animatedValue as Float)
+                    statusBarColor = color
+                    navigationBarColor = color
                 }
                 duration = 600
                 start()
@@ -141,29 +173,6 @@ class IntroActivity2 : KauBaseActivity(), ViewPager.PageTransformer, ViewPager.O
     override fun onBackPressed() {
         if (viewpager.currentItem > 0) viewpager.setCurrentItem(viewpager.currentItem - 1, true)
         else finish()
-    }
-
-    override fun onPageScrollStateChanged(state: Int) = Unit
-
-    override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-        fragments[position].onPageScrolled(positionOffset)
-        if (position + 1 < fragments.size)
-            fragments[position + 1].onPageScrolled(positionOffset - 1)
-    }
-
-    override fun onPageSelected(position: Int) {
-        fragments[position].onPageSelected()
-        val hasNext = position != fragments.size - 1
-        if (barHasNext == hasNext) return
-        barHasNext = hasNext
-        next.fadeScaleTransition {
-            setIcon(
-                    if (barHasNext) GoogleMaterial.Icon.gmd_navigate_next
-                    else GoogleMaterial.Icon.gmd_done,
-                    color = Prefs.textColor
-            )
-        }
-        skip.animate().scaleXY(if (barHasNext) 1f else 0f)
     }
 
     class IntroPageAdapter(
