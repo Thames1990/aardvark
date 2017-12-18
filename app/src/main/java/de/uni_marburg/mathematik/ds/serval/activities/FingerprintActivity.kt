9ecfa2ad3621050333ab4dd894dd829b3497cbfb
Thomas Mohr <ThomasMohr1990@gmail.com>
2017-12-18 22:02:41 +0100

    Use theme settings

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/activities/FingerprintActivity.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/activities/FingerprintActivity.kt
@@ -1,20 +1,32 @@
 package de.uni_marburg.mathematik.ds.serval.activities
 
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.widget.ImageView
+import android.widget.TextView
+import ca.allanwang.kau.utils.bindView
+import ca.allanwang.kau.utils.navigationBarColor
+import ca.allanwang.kau.utils.statusBarColor
 import com.github.ajalt.reprint.core.AuthenticationFailureReason
 import com.github.ajalt.reprint.core.AuthenticationListener
 import com.github.ajalt.reprint.core.Reprint
 import de.uni_marburg.mathematik.ds.serval.R
+import de.uni_marburg.mathematik.ds.serval.utils.Prefs
 import de.uni_marburg.mathematik.ds.serval.utils.consume
-import kotlinx.android.synthetic.main.activity_fingerprint.*
 
 class FingerprintActivity : BaseActivity() {
+
+    val container: ConstraintLayout by bindView(R.id.background)
+    val title: TextView by bindView(R.id.title)
+    val fingerprint: ImageView by bindView(R.id.fingerprint)
+    val description: TextView by bindView(R.id.description)
 
     override fun backConsumer(): Boolean = consume { finishAffinity() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fingerprint)
+        theme()
         Reprint.authenticate(object : AuthenticationListener {
             override fun onSuccess(moduleTag: Int) = finish()
 
@@ -28,5 +40,14 @@ class FingerprintActivity : BaseActivity() {
                 description.text = errorMessage
             }
         })
+    }
+
+    fun theme() {
+        container.setBackgroundColor(Prefs.backgroundColor)
+        title.setTextColor(Prefs.textColor)
+        fingerprint.setColorFilter(Prefs.textColor)
+        description.setTextColor(Prefs.textColor)
+        statusBarColor = Prefs.headerColor
+        navigationBarColor = Prefs.headerColor
     }
 }
