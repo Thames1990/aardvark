0ca6a3f77657a69e16f5255fe2cb7ec82c6608dd
Thomas Mohr <ThomasMohr1990@gmail.com>
2017-12-20 09:31:49 +0100

    Theme dashboard

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/fragments/DashboardFragment.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/fragments/DashboardFragment.kt
@@ -1,9 +1,26 @@
 package de.uni_marburg.mathematik.ds.serval.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import ca.allanwang.kau.utils.bindView
 import de.uni_marburg.mathematik.ds.serval.R
+import de.uni_marburg.mathematik.ds.serval.utils.Prefs
 
 class DashboardFragment : BaseFragment() {
 
     override val layout: Int
         get() = R.layout.fragment_dashboard
+
+    private val title: TextView by bindView(R.id.title)
+    private val image: ImageView by bindView(R.id.image)
+    private val description: TextView by bindView(R.id.description)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        title.setTextColor(Prefs.textColor)
+        image.setColorFilter(Prefs.textColor)
+        description.setTextColor(Prefs.textColor)
+    }
 }
