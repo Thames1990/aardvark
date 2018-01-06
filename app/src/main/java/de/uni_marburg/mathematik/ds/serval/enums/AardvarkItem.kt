98cb41cd885ceb659e26b094d357365d2023e021
Thames1990 <ThomasMohr1990@gmail.com>
2018-01-07 00:40:10 +0100

    Change events icon

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/enums/AardvarkItem.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/enums/AardvarkItem.kt
@@ -8,6 +8,6 @@ import de.uni_marburg.mathematik.ds.serval.R
 /** Created by thames1990 on 04.12.17. */
 enum class AardvarkItem(@StringRes val titleId: Int, val icon: IIcon) {
     DASHBOARD(R.string.dashboard, GoogleMaterial.Icon.gmd_dashboard),
-    EVENTS(R.string.events, GoogleMaterial.Icon.gmd_event),
+    EVENTS(R.string.events, GoogleMaterial.Icon.gmd_info_outline),
     MAP(R.string.map, GoogleMaterial.Icon.gmd_map)
 }
