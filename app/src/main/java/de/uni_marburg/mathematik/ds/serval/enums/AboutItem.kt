97513e8a82ace16f4e6a8e1a9987a89ac1b17534
Thames1990 <ThomasMohr1990@gmail.com>
2018-01-16 20:40:21 +0100

    Validate string resource id

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/enums/AboutItem.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/enums/AboutItem.kt
@@ -1,5 +1,6 @@
 package de.uni_marburg.mathematik.ds.serval.enums
 
+import android.support.annotation.StringRes
 import com.mikepenz.community_material_typeface_library.CommunityMaterial
 import com.mikepenz.iconics.typeface.IIcon
 import de.uni_marburg.mathematik.ds.serval.R
@@ -7,6 +8,6 @@ import de.uni_marburg.mathematik.ds.serval.R
 /**
  * Created by thames1990 on 09.01.18.
  */
-enum class AboutItem(val iicon: IIcon, val linkResId: Int) {
+enum class AboutItem(val iicon: IIcon, @StringRes val linkResId: Int) {
     GITHUB(CommunityMaterial.Icon.cmd_github_circle, R.string.github_url)
 }
