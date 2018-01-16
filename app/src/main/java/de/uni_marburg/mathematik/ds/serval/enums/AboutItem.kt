7a5775349be2931f4cd60304736290b5d36e7e24
Thames1990 <ThomasMohr1990@gmail.com>
2018-01-16 20:41:09 +0100

    Conform style

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/enums/AboutItem.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/enums/AboutItem.kt
@@ -5,9 +5,9 @@ import com.mikepenz.community_material_typeface_library.CommunityMaterial
 import com.mikepenz.iconics.typeface.IIcon
 import de.uni_marburg.mathematik.ds.serval.R
 
-/**
- * Created by thames1990 on 09.01.18.
- */
-enum class AboutItem(val iicon: IIcon, @StringRes val linkResId: Int) {
-    GITHUB(CommunityMaterial.Icon.cmd_github_circle, R.string.github_url)
+enum class AboutItem(@StringRes val linkResId: Int, val iicon: IIcon) {
+    GITHUB(
+            linkResId = R.string.github_url,
+            iicon = CommunityMaterial.Icon.cmd_github_circle
+    )
 }
