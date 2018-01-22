58b8007c86d7a39821112f83641b0694c666c5b8
Thames1990 <ThomasMohr1990@gmail.com>
2018-01-22 11:52:17 +0100

    Use event position directly

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/views/MapIItem.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/views/MapIItem.kt
@@ -8,7 +8,6 @@ import ca.allanwang.kau.iitems.KauIItem
 import ca.allanwang.kau.utils.bindView
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.MapView
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import de.uni_marburg.mathematik.ds.serval.R
 import de.uni_marburg.mathematik.ds.serval.model.event.Event
@@ -28,7 +27,7 @@ class MapIItem(val event: Event) : KauIItem<MapIItem, MapIItem.ViewHolder>(
                         isMapToolbarEnabled = false
                         isClickable = false
                     }
-                    val position = LatLng(event.location.latitude, event.location.longitude)
+                    val position = event.position
                     addMarker(MarkerOptions().position(position))
                     val cameraUpdate = CameraUpdateFactory.newLatLngZoom(position, MAP_ZOOM)
                     moveCamera(cameraUpdate)
