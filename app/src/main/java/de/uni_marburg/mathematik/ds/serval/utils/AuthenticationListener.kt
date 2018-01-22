340e871a6d0377949ce0bff5a7b3ed3064d59968
Thames1990 <ThomasMohr1990@gmail.com>
2018-01-22 14:42:46 +0100

    Use application instead of specific Aardvark implementation

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/utils/AuthenticationListener.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/utils/AuthenticationListener.kt
@@ -1,18 +1,18 @@
 package de.uni_marburg.mathematik.ds.serval.utils
 
+import android.app.Application
 import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.OnLifecycleEvent
 import android.content.Intent
-import de.uni_marburg.mathematik.ds.serval.Aardvark
 import de.uni_marburg.mathematik.ds.serval.activities.FingerprintActivity
 
 /** Created by thames1990 on 03.12.17. */
-class AuthenticationListener(val aardvark: Aardvark) : LifecycleObserver {
+class AuthenticationListener(private val application: Application) : LifecycleObserver {
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun authenticate() {
-        val fingerprintIntent = Intent(aardvark, FingerprintActivity::class.java)
+        val fingerprintIntent = Intent(application, FingerprintActivity::class.java)
         fingerprintIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        aardvark.startActivity(fingerprintIntent)
+        application.startActivity(fingerprintIntent)
     }
 }
