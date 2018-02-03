0f7f1ad03908b30c0855552d5a847080ceb08f54
Thames1990 <ThomasMohr1990@gmail.com>
2018-02-03 11:25:33 +0100

    Use reflection

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/enums/LocationRequestPriority.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/enums/LocationRequestPriority.kt
@@ -7,28 +7,31 @@ import de.uni_marburg.mathematik.ds.serval.R
 enum class LocationRequestPriority(
     @StringRes val titleRes: Int,
     @StringRes val descTextRes: Int,
-    val priority: Int
+    val priorityGetter: () -> Int
 ) {
     ACCURATE(
         titleRes = R.string.location_request_priority_accurate,
         descTextRes = R.string.location_request_priority_accurate_description,
-        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        priorityGetter = LocationRequest::PRIORITY_HIGH_ACCURACY
     ),
     BLOCK_ACCURACY(
         titleRes = R.string.location_request_priority_block_accuracy,
         descTextRes = R.string.location_request_priority_block_accuracy_description,
-        priority = LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        priorityGetter = LocationRequest::PRIORITY_BALANCED_POWER_ACCURACY
     ),
     CITY_ACCURACY(
         titleRes = R.string.location_request_priority_city_accuracy,
         descTextRes = R.string.location_request_priority_city_accuracy_description,
-        priority = LocationRequest.PRIORITY_LOW_POWER
+        priorityGetter = LocationRequest::PRIORITY_LOW_POWER
     ),
     PASSIVE(
         titleRes = R.string.location_request_priority_passive,
         descTextRes = R.string.location_request_priority_passive_description,
-        priority = LocationRequest.PRIORITY_NO_POWER
+        priorityGetter = LocationRequest::PRIORITY_NO_POWER
     );
+
+    val priority: Int
+        get() = priorityGetter()
 
     companion object {
         val values = values()
