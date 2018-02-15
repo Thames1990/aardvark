66aeaeeb4b75606c1e003fe417988cb09307fe7b
Thames1990 <ThomasMohr1990@gmail.com>
2018-02-15 23:15:46 +0100

    Inline properties

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/utils/FragmentUtils.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/utils/FragmentUtils.kt
@@ -4,8 +4,8 @@ import android.content.Context
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentActivity
 
-val Fragment.currentActivity: FragmentActivity
+inline val Fragment.currentActivity: FragmentActivity
     get() = activity ?: throw IllegalStateException("Activity must not be null")
 
-val Fragment.currentContext: Context
+inline val Fragment.currentContext: Context
     get() = context ?: throw IllegalStateException("Context must not be null")
