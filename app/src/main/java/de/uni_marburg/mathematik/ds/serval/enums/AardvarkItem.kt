2c07b8ef416f6901fd763205dd85367287428dd4
Thames1990 <ThomasMohr1990@gmail.com>
2018-02-15 21:46:38 +0100

    Minor code cleanup

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/enums/AardvarkItem.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/enums/AardvarkItem.kt
@@ -6,7 +6,18 @@ import com.mikepenz.iconics.typeface.IIcon
 import de.uni_marburg.mathematik.ds.serval.R
 
 enum class AardvarkItem(@StringRes val titleRes: Int, val iicon: IIcon) {
-    DASHBOARD(R.string.dashboard, GoogleMaterial.Icon.gmd_dashboard),
-    EVENTS(R.string.events, GoogleMaterial.Icon.gmd_info_outline),
-    MAP(R.string.map, GoogleMaterial.Icon.gmd_map)
+    DASHBOARD(
+        titleRes = R.string.dashboard,
+        iicon = GoogleMaterial.Icon.gmd_dashboard
+    ),
+
+    EVENTS(
+        titleRes = R.string.events,
+        iicon = GoogleMaterial.Icon.gmd_info_outline
+    ),
+
+    MAP(
+        titleRes = R.string.map,
+        iicon = GoogleMaterial.Icon.gmd_map
+    )
 }
