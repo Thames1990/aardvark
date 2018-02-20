17468a9b3a1e7884f15ec7c241d5eedda045032c
Thames1990 <ThomasMohr1990@gmail.com>
2018-02-20 19:38:34 +0100

    Top bar is the new default

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/utils/Prefs.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/utils/Prefs.kt
@@ -1,10 +1,8 @@
 package de.uni_marburg.mathematik.ds.serval.utils
 
-import android.graphics.Color
 import ca.allanwang.kau.kotlin.lazyResettable
 import ca.allanwang.kau.kpref.KPref
 import ca.allanwang.kau.kpref.kpref
-import ca.allanwang.kau.utils.isColorVisibleOn
 import de.uni_marburg.mathematik.ds.serval.BuildConfig
 import de.uni_marburg.mathematik.ds.serval.enums.LocationRequestPriority
 import de.uni_marburg.mathematik.ds.serval.enums.MainActivityLayout
@@ -23,12 +21,6 @@ object Prefs : KPref() {
 
     val accentColor: Int
         get() = theme.accentColor
-    val accentColorForWhite: Int
-        get() = when {
-            accentColor.isColorVisibleOn(Color.WHITE) -> accentColor
-            textColor.isColorVisibleOn(Color.WHITE) -> textColor
-            else -> Theme.FRUIT_SALAD
-        }
     val backgroundColor: Int
         get() = theme.bgColor
     val headerColor: Int
@@ -46,39 +38,49 @@ object Prefs : KPref() {
     val mainActivityLayout: MainActivityLayout
         get() = MainActivityLayout(mainActivityLayoutType)
 
-    var animate: Boolean by kpref("ANIMATE", true)
-    var analytics: Boolean by kpref("USE_ANALYTICS", true)
-    var changelog: Boolean by kpref("SHOW_CHANGELOG", true)
-    var customTextColor: Int by kpref("COLOR_TEXT", Theme.PORCELAIN)
-    var customAccentColor: Int by kpref("COLOR_ACCENT", Theme.LOCHMARA)
-    var customBackgroundColor: Int by kpref("COLOR_BACKGROUND", Theme.MINE_SHAFT)
-    var customHeaderColor: Int by kpref("COLOR_HEADER", Theme.BAHAMA_BLUE)
-    var customIconColor: Int by kpref("COLOR_ICONS", Theme.PORCELAIN)
-    var debugSettings: Boolean by kpref("DEBUG_SETTINGS", BuildConfig.DEBUG)
-    var eventCount: Int by kpref("EVENT_COUNT", EVENT_COUNT)
+    var animate: Boolean by kpref(key = "ANIMATE", fallback = true)
+    var analytics: Boolean by kpref(key = "USE_ANALYTICS", fallback = true)
+    var changelog: Boolean by kpref(key = "SHOW_CHANGELOG", fallback = true)
+    var customTextColor: Int by kpref(key = "COLOR_TEXT", fallback = Theme.PORCELAIN)
+    var customAccentColor: Int by kpref(key = "COLOR_ACCENT", fallback = Theme.LOCHMARA)
+    var customBackgroundColor: Int by kpref(key = "COLOR_BACKGROUND", fallback = Theme.MINE_SHAFT)
+    var customHeaderColor: Int by kpref(key = "COLOR_HEADER", fallback = Theme.BAHAMA_BLUE)
+    var customIconColor: Int by kpref(key = "COLOR_ICONS", fallback = Theme.PORCELAIN)
+    var debugSettings: Boolean by kpref(key = "DEBUG_SETTINGS", fallback = BuildConfig.DEBUG)
+    var eventCount: Int by kpref(key = "EVENT_COUNT", fallback = EVENT_COUNT)
     var exitConfirmation: Boolean by kpref("CONFIRM_EXIT", true)
     var mapsStyleType: Int by kpref(
-        "MAPS_STYLE_TYPE",
-        0,
-        postSetter = { _: Int -> mapsStyleLoader.invalidate() })
-    var installDate: Long by kpref("INSTALL_DATE", -1L)
-    var isFirstLaunch: Boolean by kpref("IS_FIRST_LAUNCH", true)
-    var isLoggedIn: Boolean by kpref("IS_LOGGED_IN", false)
-    var kervalBaseUrl: String by kpref("KERVAL_BASE_URL", "serval.splork.de")
-    var kervalPassword: String by kpref("KERVAL_PASSWORD", "pum123")
-    var kervalPort: Int by kpref("KERVAL_PORT", 80)
+        key = "MAPS_STYLE_TYPE",
+        fallback = 0,
+        postSetter = { _: Int -> mapsStyleLoader.invalidate() }
+    )
+    var installDate: Long by kpref(key = "INSTALL_DATE", fallback = -1L)
+    var isFirstLaunch: Boolean by kpref(key = "IS_FIRST_LAUNCH", fallback = true)
+    var kervalBaseUrl: String by kpref(key = "KERVAL_BASE_URL", fallback = "serval.splork.de")
+    var kervalPassword: String by kpref(key = "KERVAL_PASSWORD", fallback = "pum123")
+    var kervalPort: Int by kpref(key = "KERVAL_PORT", fallback = 80)
     var kervalUser: String by kpref("KERVAL_USER", "pum")
-    var lastLaunch: Long by kpref("LAST_LAUNCH", -1L)
-    var locationRequestInterval: Long by kpref("LOCATION_REQUEST_INTERVAL", 60L)
-    var locationRequestFastestInterval: Long by kpref("LOCATION_REQUEST_FASTEST_INTERVAL", 10L)
-    var locationRequestPriorityType: Int by kpref("LOCATION_REQUEST_PRIORITY", 0)
-    var mainActivityLayoutType: Int by kpref("MAIN_ACTIVITY_LAYOUT_TYPE", 1)
-    var themeType: Int by kpref("THEME", 0, postSetter = { _: Int -> themeLoader.invalidate() })
-    var tintNavBar: Boolean by kpref("TINT_NAV_BAR", false)
-    var secure_app: Boolean by kpref("USE_SECURE_FLAG", false)
-    var useProgressBar: Boolean by kpref("USE_PROGRESS_BAR", false)
-    var useVibration: Boolean by kpref("USE_VIBRATION", false)
-    var useWifiADB: Boolean by kpref("USE_WIFI_ADB", false)
-    var versionCode: Int by kpref("VERSION", -1)
-    var viewpagerSwipe: Boolean by kpref("VIEWPAGER_SWIPE", false)
+    var lastLaunch: Long by kpref(key = "LAST_LAUNCH", fallback = -1L)
+    var locationRequestInterval: Long by kpref(key = "LOCATION_REQUEST_INTERVAL", fallback = 60L)
+    var locationRequestFastestInterval: Long by kpref(
+        key = "LOCATION_REQUEST_FASTEST_INTERVAL",
+        fallback = 10L
+    )
+    var locationRequestPriorityType: Int by kpref(key = "LOCATION_REQUEST_PRIORITY", fallback = 0)
+    var mainActivityLayoutType: Int by kpref(
+        key = "MAIN_ACTIVITY_LAYOUT_TYPE",
+        fallback = MainActivityLayout.TOP_BAR.ordinal
+    )
+    var themeType: Int by kpref(
+        key = "THEME",
+        fallback = 0,
+        postSetter = { _: Int -> themeLoader.invalidate() }
+    )
+    var tintNavBar: Boolean by kpref(key = "TINT_NAV_BAR", fallback = false)
+    var secure_app: Boolean by kpref(key = "USE_SECURE_FLAG", fallback = false)
+    var useProgressBar: Boolean by kpref(key = "USE_PROGRESS_BAR", fallback = false)
+    var useVibration: Boolean by kpref(key = "USE_VIBRATION", fallback = false)
+    var useWifiADB: Boolean by kpref(key = "USE_WIFI_ADB", fallback = false)
+    var versionCode: Int by kpref(key = "VERSION", fallback = -1)
+    var viewpagerSwipe: Boolean by kpref(key = "VIEWPAGER_SWIPE", fallback = false)
 }
