5719993fc9e9134ddde119b83ab0a9ecf7f8565d
Thames1990 <ThomasMohr1990@gmail.com>
2018-02-22 11:36:46 +0100

    Add detailed activity tracking

--- app/src/main/java/de/uni_marburg/mathematik/ds/serval/fragments/DashboardFragment.kt
+++ app/src/main/java/de/uni_marburg/mathematik/ds/serval/fragments/DashboardFragment.kt
@@ -6,11 +6,15 @@ import android.widget.ImageView
 import android.widget.TextView
 import ca.allanwang.kau.utils.bindView
 import ca.allanwang.kau.utils.setIcon
+import ca.allanwang.kau.utils.string
+import com.google.android.gms.location.DetectedActivity
 import com.mikepenz.google_material_typeface_library.GoogleMaterial
 import de.uni_marburg.mathematik.ds.serval.R
 import de.uni_marburg.mathematik.ds.serval.utils.Prefs
 import de.uni_marburg.mathematik.ds.serval.utils.currentContext
+import io.nlopez.smartlocation.SmartLocation
 import org.jetbrains.anko.displayMetrics
+
 
 class DashboardFragment : BaseFragment() {
 
@@ -30,5 +34,21 @@ class DashboardFragment : BaseFragment() {
             sizeDp = currentContext.displayMetrics.densityDpi
         )
         description.setTextColor(Prefs.textColor)
+
+        SmartLocation.with(currentContext).activity().start { detectedActivity ->
+            description.text = detectedActivity.typeString
+        }
     }
+
+    private inline val DetectedActivity.typeString: String
+        get() = with(currentContext) {
+            when (type) {
+                DetectedActivity.IN_VEHICLE -> string(R.string.activity_in_vehicle)
+                DetectedActivity.ON_BICYCLE -> string(R.string.activity_on_bicycle)
+                DetectedActivity.ON_FOOT -> string(R.string.activity_on_foot)
+                DetectedActivity.STILL -> string(R.string.activity_still)
+                DetectedActivity.TILTING -> string(R.string.activity_tilting)
+                else -> string(R.string.activityUnknown)
+            }
+        }
 }
