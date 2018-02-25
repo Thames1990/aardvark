b7159bea0bf37f826a9f003822223227613a9240
Thames1990 <ThomasMohr1990@gmail.com>
2018-02-25 20:37:47 +0100

    Improve fingerprint authentication

--- app/src/main/kotlin/de/uni_marburg/mathematik/ds/serval/activities/FingerprintActivity.kt
+++ app/src/main/kotlin/de/uni_marburg/mathematik/ds/serval/activities/FingerprintActivity.kt
@@ -1,47 +1,98 @@
 package de.uni_marburg.mathematik.ds.serval.activities
 
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.CardView
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import ca.allanwang.kau.utils.bindView
-import ca.allanwang.kau.utils.setIcon
+import ca.allanwang.kau.utils.*
+import com.afollestad.materialdialogs.MaterialDialog
 import com.github.ajalt.reprint.core.AuthenticationFailureReason
 import com.github.ajalt.reprint.core.AuthenticationListener
 import com.github.ajalt.reprint.core.Reprint
-import com.mikepenz.google_material_typeface_library.GoogleMaterial
+import com.mikepenz.community_material_typeface_library.CommunityMaterial
 import de.uni_marburg.mathematik.ds.serval.R
+import de.uni_marburg.mathematik.ds.serval.enums.Theme
 import de.uni_marburg.mathematik.ds.serval.utils.Prefs
-import de.uni_marburg.mathematik.ds.serval.utils.setColors
 import de.uni_marburg.mathematik.ds.serval.utils.vibrate
-import org.jetbrains.anko.displayMetrics
+import org.jetbrains.anko.find
 
 /**
  * Show a fingerprint authentication overlay.
  */
-class FingerprintActivity : BaseActivity() {
+class FingerprintActivity : AppCompatActivity() {
 
-    private val description: TextView by bindView(R.id.description)
-    private val fingerprint: ImageView by bindView(R.id.fingerprint)
-    private val title: TextView by bindView(R.id.title)
-
-    override fun backConsumer(): Boolean {
-        finishAffinity()
-        return true
-    }
+    private lateinit var iconCircle: CardView
+    private lateinit var icon: ImageView
+    private lateinit var description: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_fingerprint)
+        val fingerprintDialog: MaterialDialog = createFingerprintDialog()
+        fingerprintDialog.show()
+        authenticate(fingerprintDialog)
+    }
 
-        setColors { text(title, description) }
-        fingerprint.setIcon(
-            icon = GoogleMaterial.Icon.gmd_fingerprint,
-            color = Prefs.textColor,
-            sizeDp = displayMetrics.densityDpi
-        )
+    private fun createFingerprintDialog(): MaterialDialog {
+        val fingerprintDialog: MaterialDialog = MaterialDialog.Builder(this)
+            .customView(R.layout.dialog_confirm_fingerprint, false)
+            .canceledOnTouchOutside(false)
+            .cancelable(false)
+            .build()
 
+        with(fingerprintDialog.customView!!) {
+            setBackgroundColor(Prefs.backgroundColor.lighten(0.1f).withMinAlpha(200))
+
+            val title: TextView = find(R.id.fingerprint_title)
+            title.setTextColor(Prefs.textColor)
+
+            val subTitle: TextView = find(R.id.fingerprint_subtitle)
+            subTitle.setTextColor(Prefs.textColor)
+
+            description = find(R.id.fingerprint_description)
+            description.setTextColor(Prefs.textColor.adjustAlpha(0.5f))
+
+            iconCircle = find(R.id.fingerprint_icon_circle)
+            iconCircle.setCardBackgroundColor(Theme.LYNCH)
+
+            icon = find(R.id.fingerprint_icon)
+            icon.setIcon(icon = CommunityMaterial.Icon.cmd_fingerprint, color = Prefs.iconColor)
+
+            val usePassword: Button = find(R.id.button_use_password)
+            with(usePassword) {
+                setTextColor(Theme.LYNCH)
+                isEnabled = false
+            }
+
+            val cancel: Button = find(R.id.button_cancel)
+            with(cancel) {
+                setTextColor(Theme.LYNCH)
+                isEnabled = false
+            }
+        }
+        return fingerprintDialog
+    }
+
+    private fun authenticate(fingerprintDialog: MaterialDialog) {
         Reprint.authenticate(object : AuthenticationListener {
-            override fun onSuccess(moduleTag: Int) = finish()
+            override fun onSuccess(moduleTag: Int) {
+                iconCircle.setCardBackgroundColor(Theme.PERSIAN_GREEN)
+                icon.fadeScaleTransition {
+                    setIcon(
+                        icon = CommunityMaterial.Icon.cmd_check,
+                        color = Prefs.iconColor
+                    )
+                }
+                description.setTextWithFade(
+                    text = "Fingerprint recognized",
+                    duration = 500L,
+                    onFinish = {
+                        fingerprintDialog.dismiss()
+                        finish()
+                    }
+                )
+            }
 
             override fun onFailure(
                 failureReason: AuthenticationFailureReason?,
@@ -50,9 +101,12 @@ class FingerprintActivity : BaseActivity() {
                 moduleTag: Int,
                 errorCode: Int
             ) {
-                description.text = errorMessage
+                iconCircle.setCardBackgroundColor(Theme.POMEGRENADE)
+                icon.setIcon(icon = CommunityMaterial.Icon.cmd_exclamation, color = Prefs.iconColor)
+                description.setTextWithFade(errorMessage.toString())
                 if (Prefs.useVibration) vibrate()
             }
         })
     }
+
 }
--- app/src/main/kotlin/de/uni_marburg/mathematik/ds/serval/enums/Theme.kt
+++ app/src/main/kotlin/de/uni_marburg/mathematik/ds/serval/enums/Theme.kt
@@ -79,13 +79,16 @@ enum class Theme(
         operator fun invoke(index: Int) = values()[index]
 
         const val ALABASTER = 0xfffafafa.toInt()
+        const val BAHAMA_BLUE = 0xff01579b.toInt()
         const val CHAMBRAY = 0xff2e4b86.toInt()
         const val FRUIT_SALAD = 0xff4CAF50.toInt()
+        const val LOCHMARA = 0xff0288d1.toInt()
+        const val LYNCH = 0xff607D8B.toInt()
         const val MINE_SHAFT = 0xff303030.toInt()
         const val PASTEL_GREEN = 0xff80E27E.toInt()
+        const val PERSIAN_GREEN = 0xff009688.toInt()
+        const val POMEGRENADE = 0xffF4511E.toInt()
         const val PORCELAIN = 0xffeceff1.toInt()
-        const val LOCHMARA = 0xff0288d1.toInt()
-        const val BAHAMA_BLUE = 0xff01579b.toInt()
 
         const val AARDVARK_GREEN = FRUIT_SALAD
     }
