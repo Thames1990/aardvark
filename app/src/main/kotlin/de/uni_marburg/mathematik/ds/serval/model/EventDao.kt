b93e793cd07a92f3add92fac619408aaff92d490
Thames1990 <ThomasMohr1990@gmail.com>
2018-03-04 22:33:25 +0100

    Explicitly define language

--- app/src/main/kotlin/de/uni_marburg/mathematik/ds/serval/model/EventDao.kt
+++ app/src/main/kotlin/de/uni_marburg/mathematik/ds/serval/model/EventDao.kt
@@ -5,19 +5,24 @@ import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
+import org.intellij.lang.annotations.Language
 
 @Dao
 abstract class EventDao {
 
+    @Language("RoomSql")
     @Query("SELECT COUNT(*) FROM events")
     abstract fun count(): Int
 
+    @Language("RoomSql")
     @Query("SELECT * FROM events WHERE id = :id")
     abstract fun getById(id: String): Event
 
+    @Language("RoomSql")
     @Query("SELECT * FROM events")
     abstract fun getAll(): List<Event>
 
+    @Language("RoomSql")
     @Query("SELECT * FROM events")
     abstract fun getAllPaged(): DataSource.Factory<Int, Event>
 
