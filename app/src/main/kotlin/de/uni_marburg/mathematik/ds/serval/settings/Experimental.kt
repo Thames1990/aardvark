f39dda6f700c158a5403a93de47ba3055300d630
Thames1990 <ThomasMohr1990@gmail.com>
2018-03-12 17:58:42 +0100

    Fix WiFiADB

--- app/src/main/kotlin/de/uni_marburg/mathematik/ds/serval/settings/Experimental.kt
+++ app/src/main/kotlin/de/uni_marburg/mathematik/ds/serval/settings/Experimental.kt
@@ -7,6 +7,7 @@ import ca.allanwang.kau.kpref.activity.KPrefAdapterBuilder
 import ca.allanwang.kau.kpref.activity.items.KPrefItemBase
 import ca.allanwang.kau.kpref.kpref
 import ca.allanwang.kau.utils.shareText
+import ca.allanwang.kau.utils.string
 import de.uni_marburg.mathematik.ds.serval.R
 import de.uni_marburg.mathematik.ds.serval.activities.SettingsActivity
 import de.uni_marburg.mathematik.ds.serval.utils.isDebugBuild
@@ -18,21 +19,12 @@ import java.math.BigInteger
 import java.net.InetAddress
 
 object ExperimentalPrefs : KPref() {
-    var enabled: Boolean by kpref(
-        key = "EXPERIMENTAL_SETTINGS_ENABLED",
-        fallback = isDebugBuild
-    )
+    var enabled: Boolean by kpref(key = "EXPERIMENTAL_SETTINGS_ENABLED", fallback = isDebugBuild)
     var secureApp: Boolean by kpref(key = "SECURE_APP", fallback = false)
-    var showDownloadProgress: Boolean by kpref(
-        key = "SHOW_DOWNLOAD_PROGRESS",
-        fallback = false
-    )
+    var showDownloadProgress: Boolean by kpref(key = "SHOW_DOWNLOAD_PROGRESS", fallback = false)
     var wifiADBEnabled: Boolean by kpref(key = "WIFI_ADB_ENABLED", fallback = false)
     var vibrationsEnabled: Boolean by kpref(key = "VIBRATIONS_ENABLED", fallback = false)
-    var viewpagerSwipeEnabled: Boolean by kpref(
-        key = "VIEWPAGER_SWIPE_ENABLED",
-        fallback = false
-    )
+    var viewpagerSwipeEnabled: Boolean by kpref(key = "VIEWPAGER_SWIPE_ENABLED", fallback = false)
 }
 
 fun SettingsActivity.experimentalItemBuilder(): KPrefAdapterBuilder.() -> Unit = {
@@ -42,15 +34,37 @@ fun SettingsActivity.experimentalItemBuilder(): KPrefAdapterBuilder.() -> Unit =
         builder = { descRes = R.string.preference_experimental_disclaimer_desc }
     )
 
-    header(R.string.preference_experimental_wifi_adb)
+    header(title = R.string.preference_experimental_wifi_adb)
+
+    fun toggleWiFiADB(activate: Boolean) {
+        val runtime: Runtime = getRuntime()
+        val superUser: Process = runtime.exec("su")
+        val outputStream = DataOutputStream(superUser.outputStream)
+        with(outputStream) {
+            if (activate) writeBytes(string(R.string.preference_experimental_wifi_adb_activate))
+            else writeBytes(string(R.string.preference_experimental_wifi_adb_deactivate))
+            writeBytes(string(R.string.preference_experimental_wifi_adb_stop))
+            writeBytes(string(R.string.preference_experimental_wifi_adb_start))
+            flush()
+            close()
+        }
+        superUser.waitFor()
+    }
+
+    fun shareWifiAdbCommand() {
+        val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+        val ipAddress: Int = wifiManager.connectionInfo.ipAddress
+        val addr: ByteArray = BigInteger.valueOf(ipAddress.toLong()).toByteArray().reversedArray()
+        val hostAddress: String = InetAddress.getByAddress(addr).hostAddress
+        shareText("adb connect $hostAddress")
+    }
 
     checkbox(
         title = R.string.preference_experimental_enable_wifi_adb,
         getter = ExperimentalPrefs::wifiADBEnabled,
-        setter = { enableWifiADB ->
-            ExperimentalPrefs.wifiADBEnabled = enableWifiADB
-            if (enableWifiADB) enableWifiAdb()
-            else disableWifiAdb()
+        setter = { wifiADBEnabled ->
+            ExperimentalPrefs.wifiADBEnabled = wifiADBEnabled
+            toggleWiFiADB(activate = wifiADBEnabled)
             reloadByTitle(R.string.preference_experimental_share_wifi_adb_command)
         }
     )
@@ -61,7 +75,7 @@ fun SettingsActivity.experimentalItemBuilder(): KPrefAdapterBuilder.() -> Unit =
             descRes = R.string.preference_experimental_share_adb_command_desc
             enabler = ExperimentalPrefs::wifiADBEnabled
             onDisabledClick = {
-                snackbarThemed(getString(R.string.preference_experimental_requires_wifi_adb))
+                snackbarThemed(string(R.string.preference_experimental_requires_wifi_adb))
             }
             onClick = { shareWifiAdbCommand() }
         }
@@ -121,39 +135,4 @@ fun SettingsActivity.experimentalItemBuilder(): KPrefAdapterBuilder.() -> Unit =
         builder = { descRes = R.string.preference_experimental_paging_desc }
     )
 
-}
-
-/** Enables WifiADB and lets the user send ADB connection information. */
-private fun enableWifiAdb() {
-    with(getRuntime().exec("su")) {
-        with(DataOutputStream(outputStream)) {
-            writeBytes("setprop service.adb.tcp.port 5555\\nstop adbd\\nstart adbd\\nexit")
-            flush()
-            close()
-        }
-        waitFor()
-    }
-}
-
-/**
- * Disables WifiADB.
- */
-private fun disableWifiAdb() {
-    with(getRuntime().exec("su")) {
-        with(DataOutputStream(outputStream)) {
-            writeBytes("setprop service.adb.tcp.port -1\\nstop adbd\\nstart adbd\\nexit")
-            flush()
-            close()
-        }
-        waitFor()
-    }
-}
-
-/** Share information to connect to the device via WifiADB. */
-private fun SettingsActivity.shareWifiAdbCommand() {
-    val wifiManager = applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
-    val ipAddress: Int = wifiManager.connectionInfo.ipAddress
-    val addr: ByteArray = BigInteger.valueOf(ipAddress.toLong()).toByteArray().reversedArray()
-    val hostAddress: String = InetAddress.getByAddress(addr).hostAddress
-    shareText("adb connect $hostAddress")
 }
