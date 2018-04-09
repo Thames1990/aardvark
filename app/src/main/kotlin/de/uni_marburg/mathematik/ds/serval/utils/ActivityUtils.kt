435c882ac82b1cc0ca865debdd66d76eacbd906e
Thomas Mohr <ThomasMohr1990@gmail.com>
2018-04-09 10:22:01 +0200

    Remove unused import statements

--- app/src/main/kotlin/de/uni_marburg/mathematik/ds/serval/utils/ActivityUtils.kt
+++ app/src/main/kotlin/de/uni_marburg/mathematik/ds/serval/utils/ActivityUtils.kt
@@ -7,8 +7,6 @@ import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.support.annotation.ColorInt
 import android.support.annotation.MenuRes
-import android.support.annotation.StringRes
-import android.support.design.widget.Snackbar
 import android.support.v4.app.FragmentActivity
 import android.support.v7.widget.Toolbar
 import android.view.Menu
